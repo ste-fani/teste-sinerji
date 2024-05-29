894ee90202a650d0d0854db7e16f131f71504502
Stefani <stefani.barros28@gmail.com>
2024-05-29 00:33:53 -0300

    organizando métodos do UsuarioRepository no arquivo UsuarioService

---
+++ testeSinerji/src/main/java/com/teste/services/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.teste.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.teste.entities.Usuario;
+import com.teste.repositories.UsuarioRepository;
+import com.teste.utils.Transactional;
+
+public class UsuarioService implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+	public Usuario salvarUsuario(Usuario usuario) {
+		return usuarioRepository.salvar(usuario);
+	}
+    
+    @Transactional
+    public List<Usuario> exibirUsuarios(){
+    	return usuarioRepository.exibirUsuarios();
+    }
+}
