e8d036f5fc9f697425600f04d9ead70ce8d2f002
Stefani <stefani.barros28@gmail.com>
2024-05-29 01:19:38 -0300

    adicionando arquivos beans e UsuarioBean

---
+++ testeSinerji/src/main/java/com/teste/controller/UsuarioBean.java
@@ -0,0 +1,54 @@
+package com.teste.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.teste.entities.TipoSexo;
+import com.teste.entities.Usuario;
+import com.teste.services.UsuarioService;
+
+@Named
+@ViewScoped
+public class UsuarioBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Usuario usuario = new Usuario();
+	private List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+
+	@Inject
+	private UsuarioService usuarioService;
+	
+	
+	public String salvarUsuario() {
+		usuarioService.salvarUsuario(usuario);
+		return "cadastroendereco";
+	}
+	
+	@PostConstruct
+	public void listarUsuarios() {
+		listaUsuarios = usuarioService.exibirUsuarios();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getListaUsuarios() {
+		return listaUsuarios;
+	}
+	
+	public TipoSexo[] getTipoSexo() {
+		return TipoSexo.values();
+	}
+}
