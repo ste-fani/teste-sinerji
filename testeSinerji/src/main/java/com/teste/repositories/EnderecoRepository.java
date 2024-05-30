fe3838e0dd7b48cff60cb05ec744a6589bc1e664
Stefani <stefani.barros28@gmail.com>
2024-05-30 12:45:02 -0300

    adicionando métodos de buscar por id, salvar e excluir endereço

---
+++ testeSinerji/src/main/java/com/teste/repositories/EnderecoRepository.java
@@ -0,0 +1,37 @@
+package com.teste.repositories;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.teste.entities.Endereco;
+
+public class EnderecoRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+	
+	public EnderecoRepository(){
+		
+	}
+	
+	public EnderecoRepository(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public Endereco buscarPorId(Long id) {
+		return manager.find(Endereco.class, id);
+	}
+	
+	public Endereco salvar(Endereco endereco) {
+		return manager.merge(endereco);
+	}
+	
+	public void excluir(Endereco endereco) {
+		endereco = buscarPorId(endereco.getId());
+		manager.remove(endereco);
+	}
+}
